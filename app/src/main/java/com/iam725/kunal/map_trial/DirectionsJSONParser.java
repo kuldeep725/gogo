51fbeb4996ad3b26116f00b03fdeb25f1f1c5c8c
kuldeepsinghbhandari <kunal725725@gmail.com>
2017-09-06 23:24:13 +0530

    1.SIGNING OUT DONE, 2. BACK BUTTON ISSUE FIXED, 3. GEOCODER IS WORKING (BUT LITTLE SLOWLY)

---
+++ app/src/main/java/com/iam725/kunal/map_trial/DirectionsJSONParser.java
@@ -0,0 +1,10 @@
+package com.iam725.kunal.map_trial;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kunal on 6/9/17.
+ */
+
+public class DirectionsJSONParser extends AppCompatActivity {
+}
